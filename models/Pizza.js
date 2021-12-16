69768dea213c63cdc6bd21dfb6ac8d0415295bf5
Adam Tilly <tillyjadam@gmail.com>
2021-12-15 21:14:34 -0600

    18-5-5

--- models/Pizza.js
+++ models/Pizza.js
@@ -4,10 +4,14 @@ const dateFormat = require('../utils/dateFormat');
 const PizzaSchema = new Schema(
   {
     pizzaName: {
-      type: String
+      type: String,
+      required: true,
+      trim: true
     },
     createdBy: {
-      type: String
+      type: String,
+      required: true,
+      trim: true
     },
     createdAt: {
       type: Date,
@@ -17,6 +21,8 @@ const PizzaSchema = new Schema(
     
     size: {
       type: String,
+      required: true,
+      enum: ['Personal', 'Small', 'Medium', 'Large', 'Extra Large'],
       default: 'Large'
     },
     toppings: [],
@@ -42,7 +48,7 @@ const Pizza = model('Pizza', PizzaSchema);
 
 // get total count of comments and replies on retrieval
 PizzaSchema.virtual('commentCount').get(function() {
-  return this.comments.length;
+  return this.comments.reduce((total, comment) => total + comment.replies.length + 1, 0);
 });
 // export the Pizza model
 module.exports = Pizza;
